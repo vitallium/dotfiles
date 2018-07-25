4bb4db94da53244359bb7c58f1fee5b65b2e0e9e
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2018-07-25 22:57:24 +0300

    [fish] update fish.config

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,9 @@
-source ~/.asdf/asdf.fish
 source ~/.config/fish/aliases.fish
 source ~/.config/fish/chromium.fish
+status --is-interactive; and source (rbenv init -|psub)
 
 # Disable fish greeting
 set -gx fish_greeting ''
 
 set -g fish_user_paths "$HOME/.cargo/bin" $fish_user_paths
+# eval (python -m virtualfish compat_aliases auto_activation projects)
