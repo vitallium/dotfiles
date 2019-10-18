321db1edf86032325b3ab3766c5dc9b56184e2b8
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2019-10-18 19:06:48 +0300

    Update casks

--- brew-cask.sh
+++ brew-cask.sh
@@ -8,36 +8,37 @@ brew cask install alfred
 brew cask install forklift
 brew cask install google-chrome
 brew cask install iterm2
-brew cask install tower
 
 # optional
 brew cask install acorn
-brew cask install aerial
-brew cask install bartender
-brew cask install charles
-brew cask install daisydisk
-brew cask install dash
-brew cask install firefox
-brew cask install fork
-brew cask install grammarly
-brew cask install hyperdock
-brew cask install iina
-brew cask install imageoptim
-brew cask install macdown
-brew cask install monodraw
-brew cask install ngrok
-brew cask install numi
-brew cask install paw
-brew cask install postico
-brew cask install skype
-brew cask install spotify
-brew cask install textual
-brew cask install transmission
-brew cask install visual-studio-code
+  \ aerial
+  \ bartender
+  \ charles
+  \ chromedriver
+  \ contexts
+  \ daisydisk
+  \ dash
+  \ firefox
+  \ fork
+  \ grammarly
+  \ hyperdock
+  \ iina
+  \ imageoptim
+  \ macdown
+  \ monodraw
+  \ ngrok
+  \ numi
+  \ paw
+  \ postico
+  \ skype
+  \ spotify
+  \ textual
+  \ transmission
+  \ visual-studio-code
 
 # fonts
 
-brew tap caskroom/fonts 
-brew cask install font-mononoki
+brew tap caskroom/fonts
+brew cask install font-mononoki font-source-code-pro font-cascadia
 
 brew cleanup -s
