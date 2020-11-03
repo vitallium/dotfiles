e6a9ce160a1f7220a51a18cb3e2a77c361cd6427
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2020-11-03 15:16:50 +0300

    fish: add tmux aliases

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -1,7 +1,11 @@
 abbr -a b bundle
 abbr -a br bundle exec rails
+alias t="tmux"
+alias ta="t a -t"
+alias tls="t ls"
+alias tn="t new -t"
 
 alias cat='bat'
 alias be='bundle exec'
-
 alias mbsync="mbsync -c \"$XDG_CONFIG_HOME/isync/mbsyncrc\""
+alias tx=tmuxinator
