091e27b0059ff2a9f11bb9c85f97d0820abbc93c
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2020-11-26 22:36:14 +0300

    fish: update config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,24 +1,42 @@
 set -x LC_ALL en_US.UTF-8
 
-set -gx fish_greeting              ''
-set -gx EDITOR                     'nvim'
+# fisher {{{
+if not functions -q fisher; and test -z $__FISHER_FLAG
+  set -x __FISHER_FLAG set
+  curl -sL git.io/fisher | source
+  fisher update
+end
+# }}}
 
-# fzf
-set -gx FZF_PREVIEW_COMMAND        'bat {}'
+# environment variables {{{
+set -x fish_greeting              ''
+set -x EDITOR                     nvim
+set -x GOPATH                     $HOME/go
+set -x GOBIN                      $HOME/.go/bin
+set -x GO111MODULE                on
+# }}}
 
 # TODO: Refactor to function
-test -e {$HOME}/.config/fish/aliases.fish; and . {$HOME}/.config/fish/aliases.fish
-test -e {$HOME}/.config/fish/functions.fish; and . {$HOME}/.config/fish/functions.fish
-test -e {$HOME}/.config/fish/paths.fish; and . {$HOME}/.config/fish/paths.fish
+test -e $HOME/.config/fish/aliases.fish; and . $HOME/.config/fish/aliases.fish
+test -e $HOME/.config/fish/functions.fish; and . $HOME/.config/fish/functions.fish
+test -e $HOME/.config/fish/paths.fish; and . $HOME/.config/fish/paths.fish
 
-test -e {$HOME}/.config/fish/gitlab.fish; and . {$HOME}/.config/fish/gitlab.fish
-test -e {$HOME}/.config/fish/chromium.fish; and . {$HOME}/.config/fish/chromium.fish
-test -e {$HOME}/.config/fish/crystal.fish; and . {$HOME}/.config/fish/crystal.fish
+test -e $HOME/.config/fish/gitlab.fish; and . $HOME/.config/fish/gitlab.fish
+test -e $HOME/.config/fish/chromium.fish; and . $HOME/.config/fish/chromium.fish
+test -e $HOME/.config/fish/crystal.fish; and . $HOME/.config/fish/crystal.fish
 
-. {$HOME}/.asdf/asdf.fish
+# asdf
+. $HOME/.asdf/asdf.fish
 
-kitty + complete setup fish | source
-
-pathadd ~/.local/bin
 # GIT is really slow in starship
 # eval (starship init fish)
+
+if type -q _pure_prompt_git
+  set -g async_prompt_functions _pure_prompt_git
+end
+
+# colors {{{
+# if test -e $HOME/.config/fish/theme.fish
+#  . $HOME/.config/fish/theme.fish
+# end
+# }}}
--- .config/fish/paths.fish
+++ .config/fish/paths.fish
@@ -1,12 +1,11 @@
 # brew
+
 pathadd /usr/local/sbin
 
 # asdf
 pathadd ~/.asdf/shims
 
 # golang
-set -gx GOPATH $HOME/go
-set -gx GOBIN $GOPATH/bin
 pathadd $GOBIN
 
 # rust
@@ -14,4 +13,7 @@ pathadd ~/.cargo/bin
 
 # jsvu
 pathadd ~/.jsvu
+
+pathadd ~/.local/bin
+pathadd ~/.go/bin
 
