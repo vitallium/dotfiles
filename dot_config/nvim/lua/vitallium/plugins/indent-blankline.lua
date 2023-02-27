cba80993b1f0d94fc969c4a242cb412704af9d1c
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2023-02-27 11:18:44 +0100

    nvim: update packages

--- dot_config/nvim/lua/vitallium/plugins/indent-blankline.lua
+++ dot_config/nvim/lua/vitallium/plugins/indent-blankline.lua
@@ -7,6 +7,6 @@ return {
     filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
     show_trailing_blankline_indent = false,
     show_current_context = true,
-    show_current_context_start = true,
+    show_current_context_start = false,
   },
 }
