bc0fbc3d24aa6b7c7779724b91cc158d75705e1c
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2023-03-13 12:59:02 +0100

    fish: remove catppuccin leftovers

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -34,13 +34,7 @@ if command -sq gdircolors
 end
 
 if command -qa bat
-  alias --save cat=bat
+  alias cat=bat
   set -Ux MANPAGER "sh -c 'col -bx | bat -l man -p'"
 end
 
-if command -qs fzf
-  set -Ux FZF_DEFAULT_OPTS "\
---color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
---color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
---color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
-end
