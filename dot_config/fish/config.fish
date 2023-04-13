97e335ced2de5c96cd7029f2b0de50f892856746
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2023-04-13 21:51:23 +0200

    fish: add fzf goodies

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -43,3 +43,9 @@ if command -qa bat
   set -Ux MANPAGER "sh -c 'col -bx | bat -l man -p'"
 end
 
+if command -qs fzf
+  set -Ux FZF_DEFAULT_OPTS "\
+		--color=bg:#000000,bg+:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+		--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+		--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+end
