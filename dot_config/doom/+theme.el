1a4db2e6e8590f228e532fc23a641e7266eef907
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2023-04-17 14:08:55 +0200

    doom: nuke emacs

--- dot_config/doom/+theme.el
+++
@@ -1,8 +0,0 @@
-;;; $DOOMDIR/+ruby.el -*- lexical-binding: t; -*-
-
-;; Visual Fill Column
-(setq-default fill-column 120)
-(setq fill-column 120
-      visual-fill-column-center-text t
-      visual-fill-column-width fill-column)
-(add-hook! 'prog-mode-hook #'display-fill-column-indicator-mode)
