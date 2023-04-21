ae81a01dadbd518c9db6276c7333844a1e44760e
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2023-04-21 16:28:24 +0200

    nvim: disable prettier from some file types

--- dot_config/nvim/lua/vitallium/plugins/prettier.lua
+++ dot_config/nvim/lua/vitallium/plugins/prettier.lua
@@ -5,19 +5,12 @@ return {
   opts = {
     bin = "prettierd", -- or `"prettier"`
     filetypes = {
-      "css",
-      "graphql",
       "html",
       "javascript",
       "javascriptreact",
-      "json",
-      "less",
-      "markdown",
-      "scss",
       "typescript",
       "typescriptreact",
       "vue",
-      "yaml",
     },
   },
 }
