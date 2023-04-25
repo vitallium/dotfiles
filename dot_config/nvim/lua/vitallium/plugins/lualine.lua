c26bf1ca9e44cbb2e99555d1054f59a3fbf7a808
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2023-04-25 21:18:07 +0200

    nvim: use old config for lualine

--- dot_config/nvim/lua/vitallium/plugins/lualine.lua
+++ dot_config/nvim/lua/vitallium/plugins/lualine.lua
@@ -1,32 +1,43 @@
 return {
   "nvim-lualine/lualine.nvim", -- Modeline
-  event = "VeryLazy",
-  config = true,
-  opts = {
-    options = {
-      theme = "auto",
-      component_separators = "",
-      section_separators = "",
-      disabled_filetypes = {
-        statusline = {},
-        winbar = { "NvimTree", "NeogitCommitMessage", "NeogitStatus", "aerial" },
-      },
-    },
-    sections = {
-      lualine_c = {
-        "%=",
-        {
-          "filetype",
-          icon_only = true,
-          icon = { align = "right" },
+  config = function()
+    local branch_max_length = 30
+    local function get_branch()
+      require("lualine.components.branch.git_branch").init()
+      local branch = require("lualine.components.branch.git_branch").get_branch()
+      return string.sub(branch, math.max(string.len(branch) - branch_max_length, 0), string.len(branch))
+    end
+
+    require("lualine").setup({
+      options = {
+        theme = "auto",
+        disabled_filetypes = {
+          statusline = {},
+          winbar = { "NvimTree", "NeogitCommitMessage", "NeogitStatus", "aerial" },
         },
-        {
-          "filename",
-          file_status = false,
-          path = 1,
-        },
+        ignore_focus = {},
+        always_divide_middle = true,
+        globalstatus = true,
+        refresh = { statusline = 1000, tabline = 1000, winbar = 1000 },
       },
-    },
-    extensions = { "aerial", "lazy", "nvim-tree" },
-  },
+      sections = {
+        lualine_a = { "mode", "searchcount" },
+        lualine_b = { get_branch, "diff" },
+        lualine_c = { { "filename", path = 1, shorting_target = 70 } },
+        lualine_x = { { "diagnostics", sources = { "nvim_lsp", "nvim_diagnostic" } } },
+        lualine_y = { "filetype" },
+        lualine_z = { "location", "progress" },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = { "mode", "searchcount", get_branch, "diff" },
+        lualine_c = { { "filename", path = 1, shorting_target = 70 } },
+        lualine_x = { { "diagnostics", sources = { "nvim_lsp", "nvim_diagnostic" } } },
+        lualine_y = { "filetype", "locally", "progress" },
+        lualine_z = {},
+      },
+      tabline = {},
+      extensions = { "aerial", "lazy", "nvim-tree" },
+    })
+  end,
 }
