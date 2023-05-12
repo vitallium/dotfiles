52c7205f413189a6e5079ea87679e1184f8087ed
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2023-05-12 23:14:08 +0200

    nvim: remove dressing.nvim

--- dot_config/nvim/lua/plugins/test.lua
+++ dot_config/nvim/lua/plugins/test.lua
@@ -3,12 +3,12 @@ return {
   event = "BufEnter",
   cmd = { "TestNearest", "TestFile" },
   keys = {
-    { "<localleader>ts", ":TestNearest<CR>g", noremap = true, silent = true, desc = "Test Nearest" },
-    { "<localleader>tv", ":TestFile<CR>g", noremap = true, silent = true, desc = "Test File" },
+    { "<localleader>ts", ":TestNearest<CR>", noremap = true, silent = true, desc = "Test Nearest" },
+    { "<localleader>tv", ":TestFile<CR>", noremap = true, silent = true, desc = "Test File" },
   },
   config = function()
     vim.api.nvim_set_var("test#strategy", "neovim")
-    vim.api.nvim_set_var("test#neovim#term_position", "vert")
+    vim.api.nvim_set_var("test#neovim#term_position", "hor")
     vim.api.nvim_set_var("test#ruby#use_binstubs", "1")
   end,
 }
