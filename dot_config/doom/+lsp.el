889807789a68380fbc1e68590fe0ad608358b8cb
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2023-05-18 22:35:56 +0200

    doom: nuke

--- dot_config/doom/+lsp.el
+++
@@ -1,7 +0,0 @@
-;;; $DOOMDIR/+lsp.el -*- lexical-binding: t; -*-
-
-(use-package! lsp-ui
-    :config
-    (setq lsp-ui-doc-delay 2
-          lsp-ui-doc-max-width 80)
-    (setq lsp-signature-function 'lsp-signature-posframe))
