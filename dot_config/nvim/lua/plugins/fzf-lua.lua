c849c45d703f10fc483e931708c66771c40d73f5
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2023-05-24 23:29:32 +0200

    nvim: fix typo in fzf_lua#live_grep_glob

--- dot_config/nvim/lua/plugins/fzf-lua.lua
+++ dot_config/nvim/lua/plugins/fzf-lua.lua
@@ -17,7 +17,7 @@ return {
     { "<leader>'", "<cmd>FzfLua resume<CR>", desc = "Resume last FzfLua command" },
     { "<leader>ht", "<cmd>FzfLua help_tags<CR>", desc = "Help tags" },
     { "<leader>fr", "<cmd>FzfLua oldfiles<CR>", desc = "Recent files" },
-    { "<leader>sp", "<cmd>FzfLua live_grep_blob<CR>", desc = "Live grep" },
+    { "<leader>sp", "<cmd>FzfLua live_grep_glob<CR>", desc = "Live grep" },
     { "<leader>gb", "<cmd>FzfLua git_branches<CR>", desc = "List GIT branches" },
   },
   opts = {
