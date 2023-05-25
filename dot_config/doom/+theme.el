9dfdec561bb6fe7a6aa8c0afc7d2d44d0d66aeb8
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2023-05-25 23:14:35 +0200

    doom: format some files

--- dot_config/doom/+theme.el
+++ dot_config/doom/+theme.el
@@ -1,21 +1,21 @@
 ;;; $DOOMDIR/+theme.el -*- lexical-binding: t; -*-
 
 (setq
-      ;; Fonts
-      ;; Primary font to use
-      doom-font (font-spec :family "Berkeley Mono" :size 16)
-      ;; Non-monospace font
-      doom-variable-pitch-font (font-spec :family "ETBembo" :size 16)
-      ;; For big-font-mode
-      doom-big-font (font-spec :family (if IS-MAC "Monaco" "mononoki") :size 20)
-      ;; For unicode glyphs
-      ;; (doom-unicode-font)
-      ;; For `fixed-pitch-serif' face
-      ;; (doom-serif-font)
-      ;; Theme
-      doom-theme 'doom-one
-      ;; Setup the style of line numbers
-      display-line-numbers-type 'relative)
+ ;; Fonts
+ ;; Primary font to use
+ doom-font (font-spec :family "Berkeley Mono" :size 16)
+ ;; Non-monospace font
+ doom-variable-pitch-font (font-spec :family "ETBembo" :size 16)
+ ;; For big-font-mode
+ doom-big-font (font-spec :family (if IS-MAC "Monaco" "mononoki") :size 20)
+ ;; For unicode glyphs
+ ;; (doom-unicode-font)
+ ;; For `fixed-pitch-serif' face
+ ;; (doom-serif-font)
+ ;; Theme
+ doom-theme 'doom-one
+ ;; Setup the style of line numbers
+ display-line-numbers-type 'relative)
 
 (after! doom-themes
   (setq doom-themes-enable-bold t
@@ -26,4 +26,6 @@
   (doom-themes-org-config))
 
 (after! treemacs
-  (add-hook 'treemacs-mode #'treemacs-follow-mode))
+  (add-hook 'treemacs-mode #'treemacs-follow-mode)
+  (setq doom-themes-treemacs-enable-variable-pitch nil
+        doom-themes-treemacs-theme "doom-colors"))
