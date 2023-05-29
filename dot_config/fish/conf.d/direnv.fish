f30b641dcdd33c6f23d73aff1b4a5d7d050d6f31
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2023-05-29 14:18:16 +0200

    fish: move direnv to conf.d

---
+++ dot_config/fish/conf.d/direnv.fish
@@ -0,0 +1,4 @@
+# put anything needed for a non-interactive shell before this
+if status is-interactive; and command -qa direnv
+  direnv hook fish | source
+end
--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -9,18 +9,6 @@ end
 set -gx LSP_USE_PLISTS             true
 set -gx RIPGREP_CONFIG_PATH       "$HOME/.ripgreprc"
 
-set configs aliases paths
-for config in $configs
-  if test -e $__fish_config_dir/$config.fish
-    . $__fish_config_dir/$config.fish
-  end
-end
-set -e configs
-
-if command -qa direnv
-  direnv hook fish | source
-end
-
 if test -e $HOME/.asdf
   . $HOME/.asdf/asdf.fish
 end
