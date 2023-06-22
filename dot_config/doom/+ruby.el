4d7532ef1793231944547324946792d684f726b1
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2023-06-22 19:30:33 +0200

    doom: add haml-lint for flycheck

--- dot_config/doom/+ruby.el
+++ dot_config/doom/+ruby.el
@@ -11,3 +11,26 @@
 
 ;; Treat underscores as part of words
 (add-hook! ruby-mode #'(lambda() (modify-syntax-entry ?_ "w" ruby-mode-syntax-table)))
+
+(after! haml-mode
+  (after! flycheck
+    (flycheck-define-checker haml-lint
+        "A haml syntax checker using the haml-lint tool."
+        :command ("bundle"
+                  "exec"
+                  "haml-lint"
+                  source-inplace)
+        :working-directory flycheck-ruby--find-project-root
+        :error-patterns
+        ((info line-start (file-name) ":" line " [C] " (message) line-end)
+        (warning line-start (file-name) ":" line " [W] " (message) line-end)
+        (error line-start (file-name) ":" line " [" (or "E" "F") "] " (message) line-end))
+        :modes (haml-mode))
+      (add-to-list 'flycheck-checkers 'haml-lint)
+      (flycheck-add-next-checker 'haml '(warning . haml-lint))
+
+      (add-to-list 'compilation-error-regexp-alist-alist
+                   '(haml-lint
+                     "^\\([^:]+\\):\\([0-9]+\\) \\[\\(W\\|E\\)\\] "
+                     1 2))
+      (add-to-list 'compilation-error-regexp-alist 'haml-lint)))
