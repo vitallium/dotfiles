4803322c1d48bb13947ba6880e7262e2574e51b6
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2023-07-11 23:03:04 +0200

    fish: configure fzf

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -105,6 +105,20 @@ gpgconf --launch gpg-agent
 set -x GPG_TTY (tty)
 set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
 
+# fzf
+set -gx FZF_DEFAULT_OPTS --cycle --filepath-word --height=50% --info=hidden --border=sharp
+
+if functions -q fzf_configure_bindings
+    set fzf_preview_file_cmd bat --line-range :100 --color=always --plain
+    set fzf_preview_dir_cmd lsd --tree --depth=1
+
+    set fzf_fd_opts --hidden
+    set fzf_dir_opts --bind 'ctrl-e:execute(command $EDITOR {} >/dev/tty)'
+
+    # Bind Ctrl-t to use fzf for the current directory.
+    fzf_configure_bindings --directory=\ct
+end
+
 # Aliases
 alias t="tmux"
 alias ta="t a -t"
