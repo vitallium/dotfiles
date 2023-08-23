c6208107ef7a116d17bc443a224f7e7d4c1d0f89
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2023-08-23 17:28:58 +0200

    rust: enable CARGO_NET_GIT_FETCH_WITH_CLI

--- dot_config/fish/conf.d/rust.fish
+++ dot_config/fish/conf.d/rust.fish
@@ -1,5 +1,8 @@
 status is-interactive || exit
 
+# https://doc.rust-lang.org/cargo/reference/environment-variables.html
+set -gx CARGO_NET_GIT_FETCH_WITH_CLI true
+
 # Rust
 if type -q sccache
     set -gx RUSTC_WRAPPER sccache
