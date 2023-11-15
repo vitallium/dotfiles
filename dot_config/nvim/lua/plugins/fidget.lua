299a6f985bc03b6d54ce92e29130932aecff77d2
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2023-11-15 19:28:11 +0100

    nvim: update packages

--- dot_config/nvim/lua/plugins/fidget.lua
+++ dot_config/nvim/lua/plugins/fidget.lua
@@ -1,12 +1,8 @@
 return {
   -- Spinner to show when LSP Server starts
   "j-hui/fidget.nvim",
-  tag = "legacy",
   event = "LspAttach",
   opts = {
-    sources = {
-      ["null-ls"] = { ignore = true },
-    },
     text = { spinner = "dots" },
   },
 }
