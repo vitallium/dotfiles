d3ba21af90d4451185de43d085cad806694bd934
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2023-11-20 20:04:55 +0100

    doom: enable theme config for treemacs

--- dot_config/doom/+theme.el
+++ dot_config/doom/+theme.el
@@ -54,7 +54,8 @@
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
   ;; Corrects (and improves) org-mode's native fontification.
-  (doom-themes-org-config))
+  (doom-themes-org-config)
+  (doom-themes-treemacs-config))
 
 (after! treemacs
   (add-hook! 'treemacs-mode-hook #'treemacs-follow-mode)
