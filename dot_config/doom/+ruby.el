6b7ea2359f72a9bb16d63302ddcd0d6f3002e26f
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2023-12-17 15:54:53 +0100

    doom: use ruby-base mode and hooks

--- dot_config/doom/+ruby.el
+++ dot_config/doom/+ruby.el
@@ -3,13 +3,12 @@
 (when (modulep! :tools lsp)
   (after! lsp-solargraph
     ;; Add asdf installations for Ruby
-    (add-to-list 'lsp-solargraph-library-directories "~/.local/share/rtx/installs/ruby")
-    (setq lsp-solargraph-use-bundler t)))
+    (add-to-list 'lsp-solargraph-library-directories "~/.local/share/rtx/installs/ruby")))
 
-(set-docsets! 'ruby-mode "Ruby_3" "Ruby_on_Rails_7")
+(set-docsets! 'ruby-base-mode "Ruby_3" "Ruby_on_Rails_7")
 
 ;; Treat underscores as part of words
-(add-hook! 'ruby-mode-hook (modify-syntax-entry ?_ "w" ruby-mode-syntax-table))
+(add-hook! 'ruby-base-mode-hook (modify-syntax-entry ?_ "w" ruby-mode-syntax-table))
 
 (after! haml-mode
   (after! flycheck
