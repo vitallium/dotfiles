0cde09fd2980dd9b30f989f722bc452d2ab7d643
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2023-12-19 18:09:42 +0100

    doom: remove old tree-sitter config

--- dot_config/doom/+tree-sitter.el
+++
@@ -1,8 +0,0 @@
-;;; $DOOMDIR/tree-sitter.el -*- lexical-binding: t; -*-
-
-(use-package! treesit-auto
-  :if (and (require 'treesit)
-           (treesit-available-p))
-  :config
-  (setq treesit-auto-install 'prompt)
-  (global-treesit-auto-mode))
