a168736979fd7309ad65e8088e5bda31dcc2dbc3
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2024-03-07 21:25:51 +0100

    doom: disable childframe for syntax

--- dot_config/doom/init.el
+++ dot_config/doom/init.el
@@ -115,8 +115,7 @@
        vterm               ; the best terminal emulation in Emacs
 
        :checkers
-       (syntax             ; tasing you for every semicolon you forget
-        +childframe)
+       syntax              ; tasing you for every semicolon you forget
        ;;spell             ; tasing you for misspelling mispelling
        jinx                ; the spell checker of the future
        ;;grammar           ; tasing grammar mistake every you make
