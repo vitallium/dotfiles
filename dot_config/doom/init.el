398a18539c08ee54a9553813a0034d7cd998ab71
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2024-03-28 19:29:05 +0100

    doom: switch back to company

--- dot_config/doom/init.el
+++ dot_config/doom/init.el
@@ -46,10 +46,10 @@
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
-       ;; (company            ; the ultimate code completion backend
-       ;;  +childframe
-       ;;  +tabnine)
-       (corfu +orderless) ; complete with cap(f), cape and a flying feather!
+       (company            ; the ultimate code completion backend
+        +childframe
+        +tabnine)
+       ;;(corfu +orderless) ; complete with cap(f), cape and a flying feather!
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ;;ivy               ; a search engine for love and life
