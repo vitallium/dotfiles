561971827e9334723d0ba089f7c67b1e304202dc
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2024-04-03 21:40:43 +0200

    nvim: remove smart-splits

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,5 +1,4 @@
 local wezterm = require("wezterm")
-local mux = wezterm.mux
 local act = wezterm.action
 local mod = "SHIFT|SUPER"
 
@@ -8,44 +7,6 @@ wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_wid
         { Text = " " .. tab.active_pane.title .. " " },
     }
 end)
-
-local function is_vim(pane)
-    -- this is set by the plugin, and unset on ExitPre in Neovim
-    return pane:get_user_vars().IS_NVIM == "true"
-end
-
-local direction_keys = {
-    Left = "h",
-    Down = "j",
-    Up = "k",
-    Right = "l",
-    -- reverse lookup
-    h = "Left",
-    j = "Down",
-    k = "Up",
-    l = "Right",
-}
-
-local function split_nav(resize_or_move, key)
-    return {
-        key = key,
-        mods = resize_or_move == "resize" and "META" or "CTRL",
-        action = wezterm.action_callback(function(win, pane)
-            if is_vim(pane) then
-                -- pass the keys through to vim/nvim
-                win:perform_action({
-                    SendKey = { key = key, mods = resize_or_move == "resize" and "META" or "CTRL" },
-                }, pane)
-            else
-                if resize_or_move == "resize" then
-                    win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
-                else
-                    win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
-                end
-            end
-        end),
-    }
-end
 
 local fonts_configurations = {
     monolisa = {
@@ -136,15 +97,5 @@ return {
         { mods = mod, key = "[", action = wezterm.action({ ActivateTabRelative = -1 }) },
         { key = "C", mods = "CTRL", action = wezterm.action.CopyTo("ClipboardAndPrimarySelection") },
         { mods = mod, key = "d", action = wezterm.action.ShowDebugOverlay },
-        -- move between split panes
-        split_nav("move", "h"),
-        split_nav("move", "j"),
-        split_nav("move", "k"),
-        split_nav("move", "l"),
-        -- resize panes
-        split_nav("resize", "h"),
-        split_nav("resize", "j"),
-        split_nav("resize", "k"),
-        split_nav("resize", "l"),
     },
 }
