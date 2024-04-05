bab053f77a2386bd7277f3495114b2dfaeda3e7c
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2024-04-05 14:22:46 +0200

    nvim: update packages

--- dot_config/nvim/lua/plugins/cmp.lua
+++ dot_config/nvim/lua/plugins/cmp.lua
@@ -14,24 +14,6 @@ return {
       "hrsh7th/cmp-path",
       "onsails/lspkind.nvim",
       "saadparwaiz1/cmp_luasnip",
-      {
-        "tzachar/cmp-tabnine",
-        build = "./install.sh",
-        config = function()
-          local tabnine = require("cmp_tabnine.config")
-          tabnine:setup({
-            max_lines = 1000,
-            max_num_results = 20,
-            sort = true,
-            run_on_every_keystroke = true,
-            snippet_placeholder = "..",
-            ignored_file_types = {
-              lua = true,
-            },
-            show_prediction_strength = false,
-          })
-        end,
-      },
     },
     config = function()
       local cmp = require("cmp")
