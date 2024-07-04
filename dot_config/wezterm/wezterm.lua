df161a7c54983a6e4f636afa3a758800199e1645
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2024-07-04 20:34:06 +0200

    wezterm: update font to MonoLisa Variable

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -10,7 +10,7 @@ end)
 
 local fonts_configurations = {
     monolisa = {
-        name = "Monolisa",
+        name = "MonoLisa Variable",
         size = 14.0,
     },
     berkeley = {
@@ -18,7 +18,7 @@ local fonts_configurations = {
         size = 16.0,
     },
 }
-local font = fonts_configurations["berkeley"]
+local font = fonts_configurations["monolisa"]
 
 local config = {}
 
