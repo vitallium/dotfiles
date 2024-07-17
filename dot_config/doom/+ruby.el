c4968d49b46e53b3f5b5fd90ec2dd82a5eb39ca7
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2024-07-17 22:40:24 +0200

    doom: update config

--- dot_config/doom/+ruby.el
+++ dot_config/doom/+ruby.el
@@ -2,16 +2,11 @@
 
 (set-docsets! 'ruby-base-mode "Ruby_3" "Ruby_on_Rails_7")
 
-(after! lsp-solargraph
-  ;; Add mise installations for Ruby
-  (add-to-list 'lsp-solargraph-library-directories "~/.local/share/mise/installs/ruby")
-  (setq lsp-solargraph-use-bundler t))
-
 ;; Treat underscores as part of words
 (add-hook! 'ruby-base-mode-hook
   (modify-syntax-entry ?_ "w" ruby-mode-syntax-table)
   ;; Disable vue-semantic-server as it takes over Ruby buffers for some reason
-  (when (modulep! :tools lsp) (setq-local lsp-disabled-clients '(vue-semantic-server)))
+  (when (modulep! :tools lsp) (setq-local lsp-disabled-clients '(vue-semantic-server ruby-ls rubocop-ls)))
   (set-lookup-handlers! 'ruby-base-mode
     :definition '(projectile-rails-goto-file-at-point robe-jump)
     :documentation #'robe-doc))
