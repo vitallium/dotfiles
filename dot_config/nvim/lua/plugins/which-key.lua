d1f222a75a5ea2723094da9692b471f80b75ffb3
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2024-07-25 07:22:37 +0200

    nvim: update which-key to v3

--- dot_config/nvim/lua/plugins/which-key.lua
+++ dot_config/nvim/lua/plugins/which-key.lua
@@ -10,45 +10,35 @@ return {
           marks = true, -- shows a list of your marks on ' and `
         },
         presets = {
-          windows = false,
+          windows = true,
           nav = true,
         },
-        triggers = 'auto', -- automatically setup triggers
       }
 
       -- Document existing key chains
-      which_key.register({
-        c = { name = 'Code' },
-        b = { name = 'Buffer' },
-        d = { name = 'Document' },
-        -- r = { name = 'Rename' },
-        s = { name = 'Search' },
-        -- w = { name = 'Workspace', _ = 'which_key_ignore' },
-        t = {
-          name = 'Toggle',
-          c = { ':set nolist!<CR>', 'Toggle invisible chars' },
-          w = { ':set wrap!<CR>', 'Toggle word wrap' },
-        },
-        g = { name = 'Git' },
-        w = {
-          name = 'Window',
-          c = { ':close<CR>', 'Close window' },
-          d = { ':close<CR>', 'Close window' },
-          s = { ':split<CR>', 'Split window' },
-          v = { ':vsplit<CR>', 'Vertically split window' },
-          h = { ':wincmd h<CR>', 'Go to window to the left' },
-          j = { ':wincmd j<CR>', 'Go to window to the bottom' },
-          k = { ':wincmd k<CR>', 'Go to window to the top' },
-          l = { ':wincmd l<CR>', 'Go to window to the right' },
-        },
-      }, { prefix = '<leader>' })
+      which_key.add {
+        { '<leader>f', group = 'File' },
+        { '<leader>c', group = 'Code' },
+        { '<leader>b', group = 'Buffer' },
+        { '<leader>d', group = 'Document' },
+        { '<leader>s', group = 'Search' },
+        -- Toggle
+        { '<leader>t', group = 'Toggle' },
+        { '<leader>tc', ':set nolist!<CR>', desc = 'Toggle invisible chars' },
+        { '<leader>tw', ':set wrap!<CR>', desc = 'Toggle word wrap' },
+        { '<leader>g', group = 'Git' },
+        -- Window
+        { '<leader>w', group = 'Window' },
+        { '<leader>wc', ':close<CR>', desc = 'Close window' },
+        { '<leader>ws', ':split<CR>', desc = 'Split window' },
+        { '<leader>wv', ':vsplit<CR>', desc = 'Vertically split window' },
+        { '<leader>wh', ':wincmd h<CR>', desc = 'Go to window to the left' },
+        { '<leader>wj', ':wincmd j<CR>', desc = 'Go to window to the bottom' },
+        { '<leader>wk', ':wincmd k<CR>', desc = 'Go to window to the top' },
+        { '<leader>wl', ':wincmd l<CR>', desc = 'Go to window to the right' },
 
-      -- With local prefix
-      which_key.register({
-        t = {
-          name = 'Test',
-        },
-      }, { prefix = '<localleader>' })
+        -- { '<localleader>t', group = 'Test' },
+      }
     end,
   },
 }
