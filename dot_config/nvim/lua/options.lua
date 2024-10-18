a38ddc086e32a8c6c3dff506f91e84df70d17bad
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2024-10-18 10:02:27 +0200

    nvim: update packages

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -54,7 +54,7 @@ vim.opt.splitbelow = true
 --  See `:help 'list'`
 --  and `:help 'listchars'`
 vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣', eol = '¬' }
 
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = 'split'
