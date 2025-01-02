b5c165908c41feb14ba1e29cb6caa596b78b2474
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2025-01-02 21:56:47 +0100

    wezterm: update config

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,14 +1,8 @@
 local wezterm = require("wezterm")
-local act = wezterm.action
-local mod = "SUPER|SHIFT"
-
-wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-    return {
-        { Text = " " .. tab.active_pane.title .. " " },
-    }
-end)
-
 local config = wezterm.config_builder()
+
+local act = wezterm.action
+local mod = "ALT|SHIFT"
 
 local function scheme_for_appearance(appearance)
     if appearance:find("Dark") then
@@ -18,7 +12,7 @@ local function scheme_for_appearance(appearance)
     end
 end
 
-wezterm.on("window-config-reloaded", function(window, pane)
+wezterm.on("window-config-reloaded", function(window)
     local overrides = window:get_config_overrides() or {}
     local appearance = window:get_appearance()
     local scheme = scheme_for_appearance(appearance)
@@ -29,13 +23,17 @@ wezterm.on("window-config-reloaded", function(window, pane)
 end)
 
 -- Font
-config.font = wezterm.font("TX-02")
+config.window_frame = {
+    font_size = 12,
+}
+config.command_palette_font_size = 16
+config.command_palette_rows = 15
+config.font = wezterm.font_with_fallback({
+    { family = "TX-02", weight = "Regular" },
+})
 config.font_size = 16.0
 config.freetype_load_target = "Light"
 config.front_end = "OpenGL"
-config.unicode_version = 15
-config.allow_square_glyphs_to_overflow_width = "Never"
-config.adjust_window_size_when_changing_font_size = false
 
 -- Colors
 config.color_scheme = "Modus-Operandi"
@@ -50,6 +48,7 @@ config.tab_bar_at_bottom = true
 
 -- Window
 config.window_close_confirmation = "NeverPrompt"
+config.adjust_window_size_when_changing_font_size = false
 
 -- Bell
 config.audible_bell = "Disabled"
@@ -70,22 +69,17 @@ config.front_end = "OpenGL"
 config.send_composed_key_when_left_alt_is_pressed = false
 config.send_composed_key_when_right_alt_is_pressed = true
 config.keys = {
+    { mods = "ALT", key = "Enter", action = act.DisableDefaultAssignment },
     { mods = mod, key = "k", action = act.ActivatePaneDirection("Up") },
     { mods = mod, key = "j", action = act.ActivatePaneDirection("Down") },
     { mods = mod, key = "l", action = act.ActivatePaneDirection("Right") },
     { mods = mod, key = "h", action = act.ActivatePaneDirection("Left") },
-    { mods = mod, key = "t", action = act.SpawnTab("CurrentPaneDomain") },
     { mods = mod, key = "|", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
     { mods = mod, key = "_", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
     { mods = mod, key = ">", action = act.MoveTabRelative(1) },
     { mods = mod, key = "<", action = act.MoveTabRelative(-1) },
     { mods = mod, key = "M", action = act.TogglePaneZoomState },
-    { mods = mod, key = "p", action = act.PaneSelect({ alphabet = "", mode = "Activate" }) },
-    { mods = mod, key = "C", action = act.CopyTo("ClipboardAndPrimarySelection") },
-    { mods = mod, key = "]", action = wezterm.action({ ActivateTabRelative = 1 }) },
-    { mods = mod, key = "[", action = wezterm.action({ ActivateTabRelative = -1 }) },
-    { key = "C", mods = "CTRL", action = wezterm.action.CopyTo("ClipboardAndPrimarySelection") },
-    { mods = mod, key = "d", action = wezterm.action.ShowDebugOverlay },
+    { mods = "SHIFT|SUPER", key = "p", action = act.ActivateCommandPalette },
     { key = "UpArrow", mods = "SHIFT", action = act.ScrollToPrompt(-1) },
     { key = "DownArrow", mods = "SHIFT", action = act.ScrollToPrompt(1) },
 }
