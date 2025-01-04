ae5a024c906d75e3a5b422af519f13ba40b8d66c
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2025-01-04 21:46:13 +0100

    fish: remove slow gpg stuff

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -102,19 +102,6 @@ end
 set -gx GOPATH $HOME/.local/go
 set -gx GO111MODULE on
 
-#
-# Gnupg
-#
-gpgconf --launch gpg-agent
-
-if test -e (gpgconf --list-dirs agent-ssh-socket)
-    set -x GPG_TTY (tty)
-    set -g -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
-else
-    echo (gpgconf --list-dirs agent-ssh-socket) "doesn't exist. Is gpg-agent running?"
-end
-
-#
 # Rust
 #
 # https://doc.rust-lang.org/cargo/reference/environment-variables.html
