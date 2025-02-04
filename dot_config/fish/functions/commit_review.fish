4d61c4850a47820cec771ac24676507d1437adf5
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2025-02-04 20:08:52 +0100

    fish: add llm function for review

---
+++ dot_config/fish/functions/commit_review.fish
@@ -0,0 +1,15 @@
+function commit_review
+    set -l start_commit $argv[1]
+    test -z "$start_commit"; and set start_commit HEAD~1
+
+    set -l temp_file (mktemp)
+    git diff --no-color "$start_commit" -- ':!sorbet/rbi/dsl' ':!sorbet/rbi/gems' ':!yarn.lock' ':!fixtures' >$temp_file
+
+    if test -s $temp_file
+        cat $temp_file | llm -m claude-3.5-sonnet -t commit_review
+    else
+        echo "No changes to review."
+    end
+
+    rm -f $temp_file
+end
