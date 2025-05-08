57f7438704e27889ab9cbfaf8c0eebd7364274fd
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2025-05-08 19:25:50 +0200

    fish: fix `is-interactive` for mise

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -63,11 +63,7 @@ end
 #
 set -gx MISE_FISH_AUTO_ACTIVATE 0
 if command -qa mise
-    if status is-interactive
-        mise activate fish | source
-    else
-        mise activate fish --shims | source
-    end
+    mise activate fish | source
 end
 
 #
