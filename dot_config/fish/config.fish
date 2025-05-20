b39b395e9ada10bfd5cb454f6c653f00653cf0d1
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2025-05-20 13:38:55 +0200

    fish: remove Intel brew

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -3,6 +3,11 @@
 #
 set fish_greeting ''
 
+#
+# Homebrew
+#
+/opt/homebrew/bin/brew shellenv | source
+
 # Paths
 fish_add_path --append \
     # Local stuff
@@ -24,65 +29,9 @@ end
 set -gx RIPGREP_CONFIG_PATH $XDG_CONFIG_HOME/ripgrep/config
 
 #
-# Homebrew
-if not set -q HOMEBREW_PREFIX
-    if test -d /opt/homebrew
-        set -Ux HOMEBREW_PREFIX /opt/homebrew
-    else if test -d /usr/local/Homebrew
-        set -Ux HOMEBREW_PREFIX /usr/local
-    end
-end
-
-if set -q HOMEBREW_PREFIX
-    # https://github.com/Homebrew/homebrew-cask/blob/master/USAGE.md
-    set -gx HOMEBREW_BAT 1
-    set -gx HOMEBREW_BUNDLE_NO_LOCK 1
-    set -gx HOMEBREW_CACHE $XDG_CACHE_HOME/brew
-    set -gx HOMEBREW_CASK_OPTS --no-quarantine
-    set -gx HOMEBREW_LOGS $XDG_CACHE_HOME/brew/logs
-    set -gx HOMEBREW_NO_ANALYTICS 1
-    set -gx HOMEBREW_NO_COMPAT 1
-    set -gx HOMEBREW_NO_ENV_HINTS 1
-
-    # https://github.com/Homebrew/homebrew-command-not-found
-    builtin source $HOMEBREW_PREFIX/Library/Taps/homebrew/homebrew-command-not-found/handler.fish 2>/dev/null
-
-    fish_add_path --append -g --move $HOMEBREW_PREFIX/{,s}bin
-    fish_add_path -g --move $HOMEBREW_PREFIX/opt/curl/bin
-    fish_add_path -g --move $HOMEBREW_PREFIX/opt/icu4c/{,s}bin
-
-    if not contains $HOMEBREW_PREFIX/share/info $INFOPATH
-        set -gx INFOPATH $HOMEBREW_PREFIX/share/info $INFOPATH
-    end
-end
-
-#
 # mise
 #
-set -gx MISE_FISH_AUTO_ACTIVATE 0
-if command -qa mise
-    if status is-interactive
-        mise activate fish | source
-    else
-        mise activate fish --shims | source
-    end
-end
-
-#
-# Eza
-#
-if type -q eza
-    alias l="eza"
-    alias ll="eza --long --header --group --created --modified -a"
-    alias ls="eza"
-end
-
-#
-# bat
-#
-if type -q bat
-    alias cat="bat --paging=never"
-end
+mise activate fish | source
 
 #
 # Golang
@@ -101,6 +50,24 @@ end
 
 if type -q sccache
     set -gx RUSTC_WRAPPER sccache
+end
+
+status is-interactive || exit
+
+#
+# Eza
+#
+if type -q eza
+    alias l="eza"
+    alias ll="eza --long --header --group --created --modified -a"
+    alias ls="eza"
+end
+
+#
+# bat
+#
+if type -q bat
+    alias cat="bat --paging=never"
 end
 
 #
