76de851dbc58716939f3947c0b99357640e3d58a
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2025-05-27 19:25:16 +0200

    Revert "doom: drop"
    This reverts commit 0b521d32af38f6fe55db152cc27be3dde80c8fd6.

---
+++ dot_config/doom/+editor.el
@@ -0,0 +1,55 @@
+;;; $DOOMDIR/+editor.el -*- lexical-binding: t; -*-
+
+;; Visual Fill Column
+(setq-default fill-column 120)
+(setq fill-column 120
+      visual-fill-column-center-text t
+      visual-fill-column-width fill-column)
+
+;; don't enable smartparens by default - when it doesn't work, it's really frustrating
+(remove-hook 'doom-first-buffer-hook #'smartparens-global-mode)
+
+(add-hook! 'prog-mode-hook
+           ;; Activate bug-reference-prog-mode
+           #'bug-reference-prog-mode
+           #'rainbow-delimiters-mode)
+;;#'display-fill-column-indicator-mode)
+
+;; Setup apheleia
+(after! apheleia
+  (setf (alist-get 'elisp-mode apheleia-mode-alist)
+        '(lisp-indent))
+  ;; Configure rubocop
+  (setf (alist-get 'rubocop apheleia-formatters)
+        '("bundle" "exec" "rubocop" "--stdin" filepath "--autocorrect"
+          "--stderr" "--format" "quiet" "--fail-level" "fatal"))
+  (setf (alist-get 'ruby-mode apheleia-mode-alist)
+        '(rubocop)))
+
+(use-package! string-inflection
+  :commands (string-inflection-all-cycle
+             string-inflection-toggle
+             string-inflection-camelcase
+             string-inflection-lower-camelcase
+             string-inflection-kebab-case
+             string-inflection-underscore
+             string-inflection-capital-underscore
+             string-inflection-upcase)
+  :init
+  (map! :leader :prefix ("c~" . "naming convention")
+        :desc "cycle" "~" #'string-inflection-all-cycle
+        :desc "toggle" "t" #'string-inflection-toggle
+        :desc "CamelCase" "c" #'string-inflection-camelcase
+        :desc "downCase" "d" #'string-inflection-lower-camelcase
+        :desc "kebab-case" "k" #'string-inflection-kebab-case
+        :desc "under_score" "_" #'string-inflection-underscore
+        :desc "Upper_Score" "u" #'string-inflection-capital-underscore
+        :desc "UP_CASE" "U" #'string-inflection-upcase)
+  (after! evil
+    (evil-define-operator evil-operator-string-inflection (beg end _type)
+      "Define a new evil operator that cycles symbol casing."
+      :move-point nil
+      (interactive "<R>")
+      (string-inflection-all-cycle)
+      (setq evil-repeat-info '([?g ?~])))
+    (define-key evil-normal-state-map (kbd "g~") 'evil-operator-string-inflection)))
--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -40,6 +40,17 @@ set -gx GOPATH $HOME/.local/go
 set -gx GO111MODULE on
 
 #
+# Emacs
+#
+# https://emacs-lsp.github.io/lsp-mode/page/performance/#use-plists-for-deserialization
+set -gx LSP_USE_PLISTS true
+
+if test -d "/Applications/Emacs.app/Contents/MacOS/bin"
+    set -x PATH "/Applications/Emacs.app/Contents/MacOS/bin" $PATH
+    alias emacs "emacs -nw" # Always launch "emacs" in terminal mode.
+end
+
+#
 # Rust
 #
 # https://doc.rust-lang.org/cargo/reference/environment-variables.html
