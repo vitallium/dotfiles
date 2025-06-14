65be4f7151410c10bbb6dbe0061b13e2fcb22c1a
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2025-06-14 21:00:28 +0200

    nvim: remove `mason`

--- dot_config/nvim/lua/plugins/lsp/init.lua
+++ dot_config/nvim/lua/plugins/lsp/init.lua
@@ -2,9 +2,6 @@ return {
   {
     "neovim/nvim-lspconfig",
     dependencies = {
-      { "mason-org/mason.nvim", opts = {} },
-      "mason-org/mason-lspconfig.nvim",
-      "WhoIsSethDaniel/mason-tool-installer.nvim",
       { "j-hui/fidget.nvim", opts = {} },
       "saghen/blink.cmp",
     },
@@ -106,19 +103,6 @@ return {
         "stylua",
         "ruby-lsp",
         "rubocop",
-      })
-      require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
-
-      require("mason-lspconfig").setup({
-        ensure_installed = {},
-        automatic_installation = false,
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-            require("lspconfig")[server_name].setup(server)
-          end,
-        },
       })
     end,
   },
